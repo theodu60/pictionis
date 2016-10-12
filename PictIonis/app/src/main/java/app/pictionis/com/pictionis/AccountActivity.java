0d566216662051d3c5716b6c70e5907d29d6b0c6
Théophile Hemachandra <hemachandra.theophile@gmail.com>
2016-10-12 08:51:17 +0200

    Log Out ok

--- PictIonis/app/src/main/java/app/pictionis/com/pictionis/AccountActivity.java
+++ PictIonis/app/src/main/java/app/pictionis/com/pictionis/AccountActivity.java
@@ -1,13 +1,46 @@
 package app.pictionis.com.pictionis;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountActivity extends AppCompatActivity {
+    private Button mLogOutBtn;
 
-    @Override
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+          @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(AccountActivity.this, MainActivity.class));
+                }
+          }
+        };
+        mLogOutBtn = (Button) findViewById(R.id.logOutBtn);
+
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+            }
+        });
+    }
+
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 }
